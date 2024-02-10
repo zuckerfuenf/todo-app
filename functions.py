a7ddff95b60979b5cf10f7517328d0c8a18c60f8
Maxim <maxim@redsquare.de>
2024-02-10 14:18:09 +0100

    Implement the edit button

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-FILEPATH = "todos_item.txt"
+FILEPATH = "todos.txt"
 
-def get_todos(filepath = "files/todos.txt"):
+def get_todos(filepath = "todos.txt"):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="files/todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
